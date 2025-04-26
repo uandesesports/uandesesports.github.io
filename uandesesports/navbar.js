af6ea57053916cebda7a15c050ec8ccc5f983ab8
uandesesports <uandesesports@gmail.com>
2025-04-25 23:30:05 -0400

    fixtures grupo A lol arreglados

---
+++ uandesesports/navbar.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.getElementById("navbar-container");
+  
+    fetch("shared/navbar.html")
+      .then(response => {
+        if (!response.ok) throw new Error("No se pudo cargar navbar.html");
+        return response.text();
+      })
+      .then(html => {
+        container.innerHTML = html;
+      })
+      .catch(error => {
+        console.error("Error al cargar la navbar:", error);
+      });
+  });
---
+++ uandesesports/pages/footer.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.getElementById("footer-container");
+  
+    fetch("footer.html")
+      .then(response => {
+        if (!response.ok) throw new Error("No se pudo cargar footer.html");
+        return response.text();
+      })
+      .then(html => {
+        container.innerHTML = html;
+      })
+      .catch(error => {
+        console.error("Error al cargar el footer:", error);
+      });
+  });
